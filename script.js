8ba6fdc8484eac00a83c45c5b182fc91643c0c90
Saroj Shrestha <saroj717@yahoo.com>
2024-01-31 15:28:06 -0700

    some updates

--- script.js
+++ script.js
@@ -1,53 +1,84 @@
 var posts = [{
   id: '1',
   body: 'Love For All, Hatred For None.',
-  user: 'Khalifatul Masih III'
+  user: 'Khalifatul Masih III',
+  comments: [{
+    id: '1',
+    body: 'Word.',
+    user: 'John Doe'
+  }]
 }, {
   id: '2',
   body: 'Change the world by being yourself.',
-  user: 'Amy Poehler'
+  user: 'Amy Poehler',
+  comments: [{
+    id: '2',
+    body: 'I am always myself, how come the world no change?',
+    user: 'John Doe'
+  }, {
+    id: '3',
+    body: 'Then change yourself.',
+    user: 'Amy Poehler'
+  }]
 }, {
   id: '3',
   body: 'Every moment is a fresh beginning.',
-  user: 'T.S Eliot'
+  user: 'T.S Eliot',
+  comments: []
 }, {
   id: '4',
   body: 'Never regret anything that made you smile.',
-  user: 'Mark Twain'
+  user: 'Mark Twain',
+  comments: [{
+    id: '4',
+    body: 'Like tipping that glass of milk over?',
+    user: 'John Doe'
+  }, {
+    id: '5',
+    body: 'Please stop trolling sir.',
+    user: 'Amy Poehler'
+  }]
 }];
 
 var Post = function Post(props) {
   var post = props.post;
-  return React.createElement(
-    'p',
-    null,
-    post.body,
-    ' - ',
-    post.user
-  );
-};
+  var comments = post.comments;
 
-var Feed = function Feed(props) {
-  var posts = props.posts.map(function (post) {
-    return React.createElement(Post, { key: post.id, post: post });
-  });
+
   return React.createElement(
     'div',
     null,
     React.createElement(
       'p',
       null,
-      'First feed'
+      post.body,
+      ' -  ',
+      post.user
     ),
-    posts,
-    React.createElement('hr', null),
-    React.createElement('hr', null),
+    comments.map(function (comment) {
+      return React.createElement(
+        'p',
+        { key: comment.id, className: 'ml-3' },
+        comment.body,
+        ' - ',
+        comment.user
+      );
+    })
+  );
+};
+
+var Feed = function Feed(props) {
+  return React.createElement(
+    'div',
+    null,
     React.createElement(
       'p',
       null,
-      'Second feed, same as first, no issues'
+      'Feed'
     ),
-    posts
+    props.posts.map(function (post) {
+      return React.createElement(Post, { key: post.id, post: post });
+    })
   );
 };
 
--- src/script.js
+++ src/script.js
@@ -2,41 +2,79 @@ const posts = [
   {
     id: '1',
     body: 'Love For All, Hatred For None.',
-    user: 'Khalifatul Masih III'
+    user: 'Khalifatul Masih III',
+    comments: [
+      {
+        id: '1',
+        body: 'Word.',
+        user: 'John Doe',
+      }
+    ]
   },{
     id: '2',
     body: 'Change the world by being yourself.',
-    user: 'Amy Poehler'
+    user: 'Amy Poehler',
+    comments: [
+      {
+        id: '2',
+        body: 'I am always myself, how come the world no change?',
+        user: 'John Doe',
+      },{
+        id: '3',
+        body: 'Then change yourself.',
+        user: 'Amy Poehler',
+      }
+    ]
   },{
     id: '3',
     body: 'Every moment is a fresh beginning.',
-    user: 'T.S Eliot'
+    user: 'T.S Eliot',
+    comments: []
   },{
     id: '4',
     body: 'Never regret anything that made you smile.',
-    user: 'Mark Twain'
+    user: 'Mark Twain',
+    comments: [
+      {
+        id: '4',
+        body: 'Like tipping that glass of milk over?',
+        user: 'John Doe',
+      },{
+        id: '5',
+        body: 'Please stop trolling sir.',
+        user: 'Amy Poehler',
+      }
+    ]
   },
-]
+];
+
+
+const Comment = (props) => {
+  const { comment } = props;
+  return <p className="ml-3">{comment.body} - {comment.user}</p>
+}
 
 
 const Post = (props) => {
-  const post = props.post;
-  return <p>{post.body} - {post.user}</p>
+  const { post } = props;
+  const { comments } = post;
+
+  return (
+  <div>
+    <p>{post.body} -  {post.user}</p>
+    {comments.map(comment => <p key={comment.id} className="ml-3">{comment.body} - {comment.user}</p>)}
+  </div>)
 }
 
 const Feed = (props) => {
-  const posts = props.posts.map(post => <Post key={post.id} post={post}/>);
   return (
     <div>
-      <h1>First feed</h1>
-      {posts}
-      <hr/>
-      <hr/>
-      <h2>Second feed, same as first, no issues</h2>
-      {posts}
-      </div>
+      <p>Feed</p>
+      {props.posts.map(post => <Post key={post.id} post={post}/>)}
+    </div>
   )
 }
+
 
 
 
